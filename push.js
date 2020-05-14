1ad1ae4c0a14f6b1a0c8bc1b40dcc7697cdd936d
tamaina <tamaina@hotmail.co.jp>
2020-05-14 17:49:58 +0900

    Update push.js

--- push.js
+++ push.js
@@ -14,8 +14,8 @@ document.getElementById('not1').addEventListener('click', () => {
     try {
     new Notification('Notification 1', {
         // tag: 'not1',
-        body: 'body a',
-        badge: 'https://determined-turing-7306bf.netlify.app/mi.svg',
+        body: 'body am',
+        // badge: 'https://determined-turing-7306bf.netlify.app/mi.svg',
         icon: 'https://join.misskey.page/files/images/imports/2019/03/forTwitter_ico_1231.webp?v=1.0.0',
         // image: 'https://determined-turing-7306bf.netlify.app/haranomachi.webp',
         silent: false
