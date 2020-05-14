f6a3a69c649a319f98f44d406ca9839777da890b
tamaina <tamaina@hotmail.co.jp>
2020-05-14 16:48:44 +0900

    Update push.js

--- push.js
+++ push.js
@@ -10,9 +10,9 @@ document.getElementById('not1').addEventListener('click', () => {
     new Notification('Notification 1', {
         tag: 'not1',
         body: 'body',
-        badge: 'https://raw.githubusercontent.com/joinmisskey/mk-assets/master/icons/mi.svg',
+        badge: 'mi.svg',
         icon: 'https://join.misskey.page/files/images/imports/2019/03/forTwitter_ico_1231.webp?v=1.0.0',
-        image: 'https://join.misskey.page/files/images/imports/2020/05/DIAGONAL_0001_BURST20200317114730908_COVER.jpg?v=1.0.0',
+        image: 'haranomachi.jpg',
         silent: false
     })
 })
