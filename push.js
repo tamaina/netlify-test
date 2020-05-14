d860589f787a5a9d9116210759dc966bfb167914
tamaina <tamaina@hotmail.co.jp>
2020-05-14 16:57:38 +0900

    Update push.js

--- push.js
+++ push.js
@@ -2,11 +2,16 @@ const push = {
     registered: Notification.permission !== 'denied' && Notification.permission !== 'default'
 }
 
+function display(text) {
+  document.getElementById('display').insertAdjacentHTML('<p>${text}</p>')
+}
+
 document.getElementById('register').addEventListener('click', () => {
     Notification.requestPermission()
 })
 
 document.getElementById('not1').addEventListener('click', () => {
+    try {
     new Notification('Notification 1', {
         tag: 'not1',
         body: 'body',
@@ -15,5 +20,8 @@ document.getElementById('not1').addEventListener('click', () => {
         image: 'haranomachi.jpg',
         silent: false
     })
+    } catch (e) {
+        display(e)
+    }
 })
 
